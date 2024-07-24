3028a62cd5e4105e50d57a245396bdd45201f904
adrian <jiga.ion.adrian@gmail.com>
2024-07-24 17:40:20 +0300

    add baseUrl, viewportHeight/viewportWidth and video config options

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
   e2e: {
+    baseUrl: 'https://demoqa.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
