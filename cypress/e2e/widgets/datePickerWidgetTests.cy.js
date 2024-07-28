6ca9aafc1c92f53a76a1be86a39ec24981dca574
Adrian Jiga <jiga.ion.adrian@gmail.com>
2024-07-28 13:42:33 +0300

    add dayjs library for easier date formatting and update datepicker tests to use it with the SelectDate custom command

--- cypress/e2e/widgets/datePickerWidgetTests.cy.js
+++ cypress/e2e/widgets/datePickerWidgetTests.cy.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 describe('Date Picker widget Suite', () => {
   beforeEach(() => {
     cy.on('uncaught:exception', (_err, _runnable) => {
@@ -8,55 +10,43 @@ describe('Date Picker widget Suite', () => {
   });
 
   it('select a date from the future', () => {
-    let date = new Date();
-    date.setDate(date.getDate() + 5);
+    const date = dayjs().add(5, 'days');
+    const futureDate = date.format('MM/DD/YYYY');
 
-    const formattedDate = date.toLocaleDateString('en-US', {
-      month: '2-digit',
-      day: '2-digit',
-      year: 'numeric',
-    });
+    cy.selectDate(futureDate);
 
-    cy.get('#datePickerMonthYearInput')
-      .should('be.visible')
-      .and('be.enabled')
-      .clear()
-      .type(`${formattedDate}{enter}`);
-
-    cy.get('#datePickerMonthYearInput').should('have.value', formattedDate);
+    cy.get('#datePickerMonthYearInput').should('have.value', futureDate);
 
     cy.get('#datePickerMonthYearInput').click();
 
     cy.get('.react-datepicker__month-select').should(
       'have.value',
-      `${date.getMonth()}`
+      date.format('M') - 1
     );
     cy.get('.react-datepicker__year-select').should(
       'have.value',
-      `${date.getFullYear()}`
+      date.format('YYYY')
     );
     cy.get('.react-datepicker__day--selected').should(
       'contain',
-      `${date.getDate()}`
+      date.format('D')
     );
   });
 
   it('leap years and rollover date', () => {
     const leapYearDate = '02/29/2024';
-    cy.get('#datePickerMonthYearInput').clear().type(`${leapYearDate}{enter}`);
+    cy.selectDate(leapYearDate);
     cy.get('#datePickerMonthYearInput').should('have.value', leapYearDate);
 
     const nonLeapYearDate = '02/29/2025';
-    cy.get('#datePickerMonthYearInput')
-      .clear()
-      .type(`${nonLeapYearDate}{enter}`);
+    cy.selectDate(nonLeapYearDate);
     cy.get('#datePickerMonthYearInput').should(
       'not.have.value',
       nonLeapYearDate
     );
 
     const rolloverDate = '04/31/2025';
-    cy.get('#datePickerMonthYearInput').clear().type(`${rolloverDate}{enter}`);
+    cy.selectDate(rolloverDate);
     cy.get('#datePickerMonthYearInput').should('have.value', '05/01/2025');
   });
 });
